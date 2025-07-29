cabcdb4b1b47c9441b65e33ebd1cdf080e061d63
shadowarchitect <krishlama69@gmail.com>
2025-07-29 21:15:40 +0700

    feat: add extended stealth referrers

--- index.js
+++ index.js
@@ -3,13 +3,41 @@ const app = express()
 const PORT = process.env.PORT
 app.get('/', (req, res) => {
   const delay = Math.floor(Math.random() * 8000) + 8000 // 8–16 detik
-  const targetURL = 'https://pitradecenter.com/chatgpt-agent-for-crypto/'
+  const urls = [
+  "https://pitradecenter.com/blockchain-assistance-dont-wait-until-your-wallet-is-locked-to-learn-how-to-get-help/",
+  "https://pitradecenter.com/how-to-start-crypto-investing-and-trading-without-getting-burned-in-2025/",
+  "https://pitradecenter.com/airdrops-and-upcoming-tokens/",
+  "https://pitradecenter.com/blockchain-ssi-5-brutal-truths-that-will-destroy-your-faith-in-centralized-identity/",
+  "https://pitradecenter.com/why-homeowners-in-oxford-ms-are-getting-crushed-by-their-insurance-policies/",
+  "https://pitradecenter.com/crypto-investing-and-trading/",
+  "https://pitradecenter.com/the-hidden-traps-inside-oxford-ms-homeowners-insurance-that-could-bankrupt-you/",
+  "https://pitradecenter.com/pi-coin-at-120k-isnt-a-dream/",
+  "https://pitradecenter.com/blockchain-4-0-how-it-compares-to-blockchain-3-0-and-earlier-versions/",
+  "https://pitradecenter.com/c-blockchain/"
+]
+const targetURL = urls[Math.floor(Math.random() * urls.length)]
+
   const fakeReferrers = [
-    'https://www.reddit.com/',
-    'https://www.pinterest.com/',
-    'https://web.whatsapp.com/',
-    'https://www.bing.com/',
-  ]
+  'https://www.linkedin.com/',
+  'https://medium.com/',
+  'https://web.whatsapp.com/',
+  'https://www.messenger.com/',
+  'https://l.facebook.com/',
+  'https://m.facebook.com/',
+  'https://t.co/',
+  'https://www.google.com/',
+  'https://duckduckgo.com/',
+  'https://t.me/',
+  'https://discord.com/',
+  'https://lemon8-app.com/',
+  'https://www.reddit.com/',
+  'https://www.pinterest.com/',
+  'https://news.ycombinator.com/',
+  'https://search.yahoo.com/',
+  'https://outlook.live.com/',
+  'https://www.quora.com/',
+  'https://mail.google.com/'
+]
   const selectedReferrer = fakeReferrers[Math.floor(Math.random() * fakeReferrers.length)]
 
   res.set('Referrer-Policy', 'no-referrer') // Optional cloaking
